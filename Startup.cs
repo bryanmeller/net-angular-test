570b2bf4e6972cf776556b963e6048d0bd5c998c
bryanmeller <bryan.meller@live.com>
2018-03-22 21:58:07 -0700

    Seeding Users and Photos

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace DatingApp
         {
             var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value);
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc();
+            services.AddTransient<Seed>();
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -47,10 +47,11 @@ namespace DatingApp
                         ValidateAudience = false
                     };
                 });
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -71,10 +72,10 @@ namespace DatingApp
                     });
                 });
             }
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseAuthentication();
             app.UseMvc();
-            
         }
     }
 }
